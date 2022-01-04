faed1e6a5b9f3e44af1276ce73011032e1446e81
Daniel Ponce <dmep17@gmail.com>
2022-01-04 13:34:02 -0500

    Update completed and Delete Todo

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -7,60 +7,23 @@
         </div>
         <div class="container relative max-w-lg pt-6 mx-auto">
             <div class="p-4 rounded-md shadow-md shadow-slate-200">
-                <div class="flex justify-between mb-2">
+                <div class="flex justify-between mb-2" v-for="(todo) in sortTodos" :key="todo.id">
                     <div class="flex">
                         <div class="flex-auto">
-                            <input type="checkbox" id="list-1" class="mr-4" />
+                            <input
+                                type="checkbox"
+                                :id="'todo-' + todo.id"
+                                class="mr-4"
+                                :checked="todo.completed"
+                                @click="updateTodo(todo, todo.title, !todo.completed)"
+                            />
                         </div>
-                        <label for="list-1">Hacer la tarea de matemática a las 10:30 de la mañana</label>
+                        <label
+                            :for="'todo-' + todo.id"
+                            :class="{ 'line-through': todo.completed }"
+                        >{{ todo.title }}</label>
                     </div>
-                    <button class="ml-4">
-                        <svg
-                            width="18px"
-                            height="18px"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <g id="Iconly/Curved/Delete">
-                                <g id="Delete">
-                                    <path
-                                        id="Stroke 1"
-                                        d="M18.8892 9.5542C18.8892 17.5732 20.0435 21.198 12.2797 21.198C4.5149 21.198 5.693 17.5732 5.693 9.5542"
-                                        stroke="#130F26"
-                                        stroke-width="1.5"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                    />
-                                    <path
-                                        id="Stroke 3"
-                                        d="M20.3651 6.47985H4.2146"
-                                        stroke="#130F26"
-                                        stroke-width="1.5"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                    />
-                                    <path
-                                        id="Stroke 5"
-                                        d="M15.7148 6.47983C15.7148 6.47983 16.2434 2.71411 12.2891 2.71411C8.33578 2.71411 8.86435 6.47983 8.86435 6.47983"
-                                        stroke="#130F26"
-                                        stroke-width="1.5"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                    />
-                                </g>
-                            </g>
-                        </svg>
-                    </button>
-                </div>
-                <div class="flex justify-between mb-2">
-                    <div class="flex">
-                        <div class="flex-auto">
-                            <input type="checkbox" id="list-1" class="mr-4" />
-                        </div>
-                        <label for="list-1">Dormir temprano</label>
-                    </div>
-                    <button class="ml-4">
+                    <button class="ml-4" @click="deleteTodo(todo)">
                         <svg
                             width="18px"
                             height="18px"
@@ -103,13 +66,121 @@
         </div>
         <div class="fixed bottom-0 w-full p-4 bg-white border-t border-gray-200">
             <div>
-                <button class="font-bold text-blue-600">+ Nuevo Recordatorio</button>
+                <button
+                    class="font-bold text-blue-600"
+                    @click="showModalTodo = !showModalTodo"
+                >+ Nuevo Recordatorio</button>
             </div>
+        </div>
+    </div>
+    <div v-show="showModalTodo" class="fixed top-0 w-full h-full overflow-hidden">
+        <div class="relative flex justify-center w-full h-full">
+            <div class="container relative z-20 max-w-xl mt-20 h-min">
+                <input
+                    type="text"
+                    placeholder="Escribe un nuevo recordatorio..."
+                    class="text-2xl shadow-[0px_20px_55px_-5px_rgba(0,0,0,0.3)] placeholder-zinc-400 h-14 bg-neutral-200 w-full rounded-xl shadow-neutral-500 border-neutral-300 focus:ring-0 focus:border-neutral-300"
+                />
+            </div>
+            <div @click="showModalTodo = !showModalTodo" class="absolute top-0 z-10 w-full h-full"></div>
         </div>
     </div>
 </template>
 <script>
-export default {
+import axios from "axios";
 
+export default {
+    data() {
+        return {
+            todos: [],
+            showModalTodo: false
+        }
+    },
+
+    mounted() {
+        const todos = null;
+
+        var config = {
+            method: 'get',
+            url: 'https://jsonplaceholder-v1.herokuapp.com/api/todos',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        };
+
+        axios(config)
+            .then(response => (this.todos = response.data.data))
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
+
+    computed: {
+        sortTodos() {
+            return this.todos.sort((a, b) => {
+                if (a.completed == true && b.completed == false) {
+
+                    return 1
+                }
+                if (a.completed == false && b.completed == true) {
+                    return -1
+                }
+                return 0
+            })
+        },
+    },
+
+    methods: {
+        updateTodo(todo, title, completed) {
+            var data = JSON.stringify({
+                "title": title,
+                "completed": completed
+            });
+
+            const index = this.todos.findIndex(e => e.id == todo.id)
+
+            var config = {
+                method: 'put',
+                url: 'https://jsonplaceholder-v1.herokuapp.com/api/todos/' + todo.id,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                data: data
+            };
+
+            axios(config)
+                .then(response => {
+                    this.todos[index].title = title
+                    this.todos[index].completed = completed
+                    console.log('succcess');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        deleteTodo(todo) {
+            var config = {
+                method: 'delete',
+                url: 'https://jsonplaceholder-v1.herokuapp.com/api/todos/' + todo.id,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }
+
+            const index = this.todos.findIndex(e => e.id == todo.id)
+
+            axios(config)
+                .then((response) => {
+                    this.todos.splice(index, 1);
+                    console.log('success');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    },
 }
 </script>
